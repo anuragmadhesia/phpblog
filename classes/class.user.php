22805ea4e06835c5612e89e7b585a716e31717bc
anurag madhesia <madhesiaanurag99@gmail.com>
2022-09-11 15:04:47 +0530

    sign mistake

--- classes/class.user.php
+++ classes/class.user.php
@@ -1,49 +1,56 @@
 <?php
-class User{
+class User
+{
     private $db;
 
-    public function __construct($db){
-        $this->db=$db;
+    public function __construct($db)
+    {
+        $this->db = $db;
     }
 
-    public function is_logged_in(){
-        if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true) {
+    public function is_logged_in()
+    {
+        if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
             return true;
         }
     }
 
-    public function create_hash($value){
-        return $hash=crypt($value,'$2a$12.substr(str_replace('+','.',base64_encode(sha1(microtime(true),true))),0,22)');
+    public function create_hash($value)
+    {
+        return $hash = crypt($value, '$2a$12.substr(str_replace(' + ',' . ',base64_encode(sha1(microtime(true),true))),0,22)');
     }
 
-    private function verify_hash($password,$hash){
+    private function verify_hash($password, $hash)
+    {
         return $hash == crypt($password, $hash);
     }
 
-    private function get_user_hash($username){
-        try{
-            $stmt=$this->db->prepare('SELECT password FROM users WHERE username=:username');
-            $stmt->execute(array('username'=>$username));
-            $row=$stmt->fetch();
+    private function get_user_hash($username)
+    {
+        try {
+            $stmt = $this->db->prepare('SELECT password FROM users WHERE username=:username');
+            $stmt->execute(array('username' => $username));
+            $row = $stmt->fetch();
             // return $row['password'];
-            $row['password'] ??= 'default value';return $row['password'];
-        }
-        catch(PDOException $e){
-            echo '<p>'.$e->getMessage().'</p>';
+            $row['password'] ?? 'default value';
+            return $row['password'];
+        } catch (PDOException $e) {
+            echo '<p>' . $e->getMessage() . '</p>';
         }
     }
 
-    public function login($username,$password){
-        $hashed=$this->get_user_hash($username);
+    public function login($username, $password)
+    {
+        $hashed = $this->get_user_hash($username);
 
-        if ($this->verify_hash($password,$hashed)==1) {
-            $_SESSION['loggedin']=true;
+        if ($this->verify_hash($password, $hashed) == 1) {
+            $_SESSION['loggedin'] = true;
             return true;
         }
     }
 
-    public function logout(){
+    public function logout()
+    {
         session_destroy();
     }
 }
-?>
